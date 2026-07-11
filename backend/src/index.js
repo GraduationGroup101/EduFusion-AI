8f45ceb404f8077519c1174ade4f0f9954b51b0c
Nizar Y Alqerem <nizaryousef01@gmail.com>
2026-07-11 20:59:04 +0300

    index-edit

--- backend/src/index.js
+++ backend/src/index.js
@@ -42,6 +42,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something went wrong' });
 });
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
